29e113b06d9f7978cee571e310b6ce9c8af93e5f
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-07-07 17:41:32 -0600

    ex19 ready

--- src/ex19/two_sum.py
+++ src/ex19/two_sum.py
@@ -3,4 +3,18 @@ from typing import List
 
 def two_sum(nums: List[int], target: int) -> List[int]:
     result = list()
+    for i, n in enumerate(nums):
+        temp_index = i
+        finding = target - n
+        exists = nums.count(finding)
+        if exists == 0:
+            continue
+        else:
+            temp_index2 = nums.index(finding)
+            if temp_index2 == temp_index:
+                continue
+            else:
+                result.append(temp_index)
+                result.append(temp_index2)
+                break
     return result
