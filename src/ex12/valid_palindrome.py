0ee3cf426efbe8622a7b0f7e40a8950277b9c99e
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-07-07 16:08:31 -0600

    ex17 ready

--- src/ex12/valid_palindrome.py
+++ src/ex12/valid_palindrome.py
@@ -5,7 +5,6 @@ def is_palindrome(s: str) -> bool:
     regex = re.compile("[^0-9a-zA-Z]")
     word = re.sub(regex, "", s).lower()
     rev_word = word[::-1]
-    print(word)
     if word == rev_word:
         return True
     return False
