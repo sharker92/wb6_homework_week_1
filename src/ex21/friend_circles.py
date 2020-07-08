be6efe799f266fa0936ea51f8f69246f5de01404
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-07-07 22:31:29 -0600

    ex21 ready, almost there :D

--- src/ex21/friend_circles.py
+++ src/ex21/friend_circles.py
@@ -2,5 +2,38 @@ from typing import List
 
 
 def find_circle_num(M: List[List[int]]) -> int:
-    result = 0
-    return result
+    result = list()
+    num_students = len(M)
+    for x, students in enumerate(M):
+        if students.count(1) == 1:
+            result.append(x)
+    for i, students in enumerate(M):
+        if i in result:
+            continue
+        union = list()
+        for j, friend in enumerate(students):
+            if i == j:
+                continue
+            else:
+                if friend == 1:
+                    M[i][j] = 0
+                    M[j][i] = 0
+                    union.append(i)
+                    union.extend(is_friend(M, j))
+        if union:
+            result.append(union)
+    return len(result)
+
+
+def is_friend(M: List[int], i: int) -> List[int]:
+    circle = list()
+    circle.append(i)
+    for j, friend in enumerate(M[i]):
+        if i == j:
+            continue
+        else:
+            if friend == 1:
+                M[i][j] = 0
+                M[j][i] = 0
+                circle.extend(is_friend(M, j))
+    return circle
