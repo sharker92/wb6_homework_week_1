07e79e87863093114d8d2460ccb542ff89cd7617
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-07-08 13:06:40 -0600

    improving ex08 and ex03 based on group session learnings

--- src/ex08/power_of_two.py
+++ src/ex08/power_of_two.py
@@ -1,11 +1,3 @@
 
 def is_power_of_two(n: int) -> bool:
-    result = True
-    binary = [d for d in str(bin(n))]
-    if binary[2] != "1":
-        return False
-    for i in range(3, len(binary)):
-        if binary[i] != "0":
-            result = False
-            break
-    return result
+    return n > 0 and not n & (n - 1)
