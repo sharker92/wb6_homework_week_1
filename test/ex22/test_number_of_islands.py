fa67672d4e1a6fd932aea87b25f8a479684a5830
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-07-07 23:24:50 -0600

    ex22 ready

--- test/ex22/test_number_of_islands.py
+++ test/ex22/test_number_of_islands.py
@@ -2,20 +2,27 @@ from wb6_homework_week_1.src.ex22.number_of_islands import num_islands
 
 
 def test_num_islands_example_01():
-    input = [[1, 1, 1, 1, 0],
-             [1, 1, 0, 1, 0],
-             [1, 1, 0, 0, 0],
-             [0, 0, 0, 0, 0]]
+    input = [["1", "1", "1", "1", "0"],
+             ["1", "1", "0", "1", "0"],
+             ["1", "1", "0", "0", "0"],
+             ["0", "0", "0", "0", "0"]]
     output = 1
 
     assert output == num_islands(input)
 
 
 def test_num_islands_example_02():
-    input = [[1, 1, 0, 0, 0],
-             [1, 1, 0, 0, 0],
-             [0, 0, 1, 0, 0],
-             [0, 0, 0, 1, 1]]
+    input = [["1", "1", "0", "0", "0"],
+             ["1", "1", "0", "0", "0"],
+             ["0", "0", "1", "0", "0"],
+             ["0", "0", "0", "1", "1"]]
+    output = 3
+
+    assert output == num_islands(input)
+
+
+def test_num_islands_example_03():
+    input = [["1", "0", "1", "1", "0", "1", "1"]]
     output = 3
 
     assert output == num_islands(input)
