cdb5ee87b1e69cd5f607744e3647a0629c1bb17d
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-07-08 01:25:31 -0600

    ex23 almost done, return not working :(

--- src/ex23/surrounded_regions.py
+++ src/ex23/surrounded_regions.py
@@ -2,4 +2,60 @@ from typing import List
 
 
 def surr_region(board: List[List[str]]) -> None:
-    result = 0
+    for i, board_row in enumerate(board):
+        if i == 0 or i + 1 == len(board):
+            continue
+        for j, board_col in enumerate(board_row):
+            if j == 0 or j + 1 == len(board[0]):
+                continue
+            if board_col == "O":
+                print("___________")
+                X = flip_board(board, i, j)
+                print("_X_X_X_X_XXX____")
+
+
+def flip_board(board: List[List[int]], i: int, j: int) -> None:
+    jump = 0
+    print(board)
+    print("i:", i, "j:", j)
+    print(board[i][j])
+
+    #x = input("")
+
+    if j + 1 < len(board[0]) and (board[i][j+1] == "X" or len(board[i][j+1]) >= 2):
+        print("one")
+        board[i][j] += "X"
+    elif j + 1 < len(board[0]) and jump == 0:
+        print("?")
+        flip_board(board, i, j+1)
+    else:
+        return 0
+
+    if i + 1 < len(board) and (board[i+1][j] == "X" or len(board[i+1][j]) >= 3):
+        print("two")
+        board[i][j] += "X"
+    elif i + 1 < len(board) and jump == 0:
+        print("???")
+        flip_board(board, i+1, j)
+    else:
+        return 0
+
+    if i - 1 >= 0 and (board[i-1][j] == "X" or len(board[i-1][j]) >= 4):
+        print("three")
+        board[i][j] += "X"
+    elif i - 1 >= 0 and jump == 0:
+        flip_board(board, i-1, j)
+    else:
+        return 0
+
+    if j - 1 >= 0 and (board[i][j-1] == "X" or len(board[i][j-1]) >= 5):
+        print("four")
+        board[i][j] += "X"
+    elif j - 1 >= 0 and jump == 0:
+        flip_board(board, i, j-1)
+    else:
+        return 0
+
+    board[i][j] = "X"
+
+    return 0
--- test/ex23/test_surrounded_regions.py
+++ test/ex23/test_surrounded_regions.py
@@ -2,15 +2,28 @@ from wb6_homework_week_1.src.ex23.surrounded_regions import surr_region
 
 
 def test_surr_region_example_01():
-    input = [["X", "X", "X", "X"]
-             ["X", "O", "O", "X"]
-             ["X", "X", "O", "X"]
+    input = [["X", "X", "X", "X"],
+             ["X", "O", "O", "X"],
+             ["X", "X", "O", "X"],
              ["X", "O", "X", "X"]]
 
-    output = [["X", "X", "X", "X"]
-              ["X", "X", "X", "X"]
-              ["X", "X", "X", "X"]
+    output = [["X", "X", "X", "X"],
+              ["X", "X", "X", "X"],
+              ["X", "X", "X", "X"],
               ["X", "O", "X", "X"]]
+    surr_region(input)
+
+    assert output == input
+
+
+def test_surr_region_example_02():
+    input = [["O", "O", "O"],
+             ["O", "O", "O"],
+             ["O", "O", "O"]]
+
+    output = [["O", "O", "O"],
+              ["O", "O", "O"],
+              ["O", "O", "O"]]
     surr_region(input)
 
     assert output == input
